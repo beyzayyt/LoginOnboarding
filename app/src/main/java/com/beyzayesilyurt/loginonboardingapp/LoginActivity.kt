9ae4c9c60031f21b8f5d99d58b1cf78e5c9764c4
Beyzanur <beyzanur.yesilyurt@kobil.com>
2021-06-15 15:19:22 +0300

    FIX: Viewbinding is refactored.

--- app/src/main/java/com/beyzayesilyurt/loginonboardingapp/LoginActivity.kt
+++ app/src/main/java/com/beyzayesilyurt/loginonboardingapp/LoginActivity.kt
@@ -4,45 +4,73 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+import com.beyzayesilyurt.loginonboardingapp.databinding.ActivityLoginBinding
 
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
     var userNameLogin = ""
     var userPasswordLogin = ""
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
-
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
-        var sharedPreferences =
+        val sharedPreferences =
             this.getSharedPreferences("com.beyzayesilyurt.loginonboardingapp", MODE_PRIVATE)
 
-        val editUsername = findViewById<EditText>(R.id.editText_name)
-        val editPassword = findViewById<EditText>(R.id.editText_password)
-        val rememberMe = findViewById<RadioButton>(R.id.radioButton_remember)
+        val editUsername = binding.editTextName
+        val editPassword = binding.editTextPassword
+        val rememberMe = binding.radioButtonRemember
 
-        var savedUserName = sharedPreferences.getString("savedUserName", "")
+        val savedUserName = sharedPreferences.getString("savedUserName", "")
         editUsername.setText(savedUserName)
 
-        val buttonLogin = findViewById<ImageButton>(R.id.imageButton_login_back)
-        buttonLogin.setOnClickListener {
+        binding.buttonLoginPageLogin.setOnClickListener {
             val route = Intent(this, MainActivity::class.java)
             startActivity(route)
         }
 
-        val textSignup = findViewById<TextView>(R.id.textView_login_signup)
-        textSignup.setOnClickListener {
+        binding.textViewLoginSignup.setOnClickListener {
             val route1 = Intent(this, SignUpActivity::class.java)
             startActivity(route1)
         }
 
-        val login = findViewById<Button>(R.id.button_loginPage_login)
-
         val userNameSignUp = sharedPreferences.getString("userNameSignup", "empty")
        //val userPasswordSignUp = sharedPreferences.getString("userPasswordSignup", "empty")
 
-        login.setOnClickListener {
+        binding.buttonLoginPageLogin.setOnClickListener {
+            userNameLogin = editUsername.text.toString()
+            userPasswordLogin = editPassword.text.toString()
+
+            if (editUsername.text.isNullOrEmpty()) {
+                editUsername.requestFocus()
+                editUsername.error = "Field can not be left blank"
+            } else if (editPassword.text.isNullOrEmpty()) {
+                editPassword.requestFocus()
+                editPassword.error = "Field can not be left blank"
+            } else if (userNameSignUp != userNameLogin) {
+                editUsername.requestFocus()
+                editUsername.error = "can not paired"
+            } else if (rememberMe.isChecked) {
+                sharedPreferences.edit().putString("savedUserName", userNameLogin).apply()
+                val route = Intent(this, HomeActivity::class.java)
+                startActivity(route)
+            } else {
+                val route = Intent(this, HomeActivity::class.java)
+                startActivity(route)
+                editUsername.setText("")
+                editPassword.setText("")
+            }
+        }
+
+
+       /* login.setOnClickListener {
             userNameLogin = editUsername.text.toString()
             userPasswordLogin = editPassword.text.toString()
 
@@ -65,6 +93,6 @@ class LoginActivity : AppCompatActivity() {
                 editUsername.setText("")
                 editPassword.setText("")
             }
-        }
+        }*/
     }
 }
